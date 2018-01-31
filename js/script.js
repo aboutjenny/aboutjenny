f98ab1a748ebaf7c36b5b840066a09c004f4940d
aboutjenny <jenny.dejesus@hotmail.com>
2018-01-31 18:18:04 +0000

    navbar work when scrolling

--- js/script.js
+++ js/script.js
@@ -6,4 +6,21 @@ $(document).ready(function() {
 
 console.log(jQuery === $);
 
-$("h1").css("color", "red");
+function toggleNavigation() {
+  // $("selector").action("arguments");
+
+  $("#burger-menu-container").slideToggle();
+  console.log("this is working");
+};
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 500) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+};
