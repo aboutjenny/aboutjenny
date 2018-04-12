cf9665c9efdc562934630aa07d2a8dfc1c4ddbbd
aboutjenny <jenny.dejesus@hotmail.com>
2018-04-13 00:04:30 +0100

    media query and mobile bar working

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ console.log(jQuery === $);
 function toggleNavigation() {
   // $("selector").action("arguments");
 
-  $("#burger-menu-container").slideToggle();
+  $(".burger-menu").slideToggle();
   console.log("this is working");
 };
+
 
 // When the user scrolls down 20px from the top of the document, slide down the navbar
 // When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
@@ -22,5 +23,13 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-60px";
+  }
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 300) {
+    document.getElementById("mobilenavbar").style.top = "0";
+  } else {
+    document.getElementById("mobilenavbar").style.top = "-70px";
   }
 };
