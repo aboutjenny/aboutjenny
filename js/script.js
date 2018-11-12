d748c920a514f284798d31daa44c45e3e581c900
aboutjenny <25507791+aboutjenny@users.noreply.github.com>
2018-11-12 21:15:55 +0000

    iPad version  working

--- js/script.js
+++ js/script.js
@@ -2,42 +2,26 @@
 $(document).ready(function() {
 
   $('#burger-menu-container').click(function() {
-           $('#burger-menu').fadeToggle("ease");
-           $('#pieceOne').toggleClass('activatePieceOne');
-           $('#pieceTwo').toggleClass('activatePieceTwo');
-           console.log("todo bien");
-           $('#pieceOut').toggleClass('activatePieceOut');
-           $('#pieceOut2').toggleClass('activatePieceOut');
-           $("body").toggleClass('overFlowHidden');
-      });
+       $('#burger-menu').fadeToggle("ease");
+       $('#pieceOne').toggleClass('activatePieceOne');
+       $('#pieceTwo').toggleClass('activatePieceTwo');
+       console.log("todo bien");
+       $('#pieceOut').toggleClass('activatePieceOut');
+       $('#pieceOut2').toggleClass('activatePieceOut');
+       $("body").toggleClass('overFlowHidden');
+    });
 
 
-// function scrollDisable() {
-//   if($('.burger-menu').html('display') == 'block'){
-//     $('body').addClass('fixedPosition');
-//   }  else if ($('.burger-menu').html('display') == 'none'){
-//     $('body').removeClass('fixedPosition');
-//   }
-// };
+  window.onscroll = function() {scrollFunction()};
 
-// function Test1() {
-//   toggleNavigation();
-//   scrollDisable();
-// }
-
-console.log("after toggle");
+  function scrollFunction() {
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 500) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-60px";
+    }
+  };
 
 
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 500) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-60px";
-  }
-};
 
 });
