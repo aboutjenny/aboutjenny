a796cb23f16738541d36f86d2c7e71086e070fdb
aboutjenny <25507791+aboutjenny@users.noreply.github.com>
2018-12-07 17:12:29 +0000

    Smooth Scrooling test1

--- js/script.js
+++ js/script.js
@@ -84,4 +84,12 @@ $(document).ready(function() {
     }
   };
 
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
+
 });
